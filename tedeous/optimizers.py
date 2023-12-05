020a8e61252219e8b43275d704df22112165c4d3
Nikita Demyanchuk <86979292+nikiniki1@users.noreply.github.com>
2023-12-05 11:12:48 +0300

    TT+ZO

--- tedeous/optimizers.py
+++ tedeous/optimizers.py
@@ -9,7 +9,7 @@ from torch.nn.utils import parameters_to_vector, vector_to_parameters
 from tedeous.device import device_type
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-
+from torch.optim.optimizer import Optimizer
 
 class PSO():
     """Custom PSO optimizer.
@@ -253,3 +253,83 @@ class PSO():
         min_loss =  torch.min(self.f_p)
 
         return min_loss
+
+
+class ZO_AdaMM(torch.optim.Optimizer):
+    def __init__(self, params, input_size, gradient_mode='forward', sampler='uniform', N_samples=1, dim=2, lr=1e-3,
+                 betas=(0.9, 0.999), mu=1e-03, eps=1e-12):
+
+        defaults = dict(lr=lr, betas=betas, mu=mu, eps=eps)
+        super().__init__(params, defaults)
+        self.input_size = input_size
+        self.gradient_mode = gradient_mode
+        self.sampler = sampler
+        self.N_samples = N_samples
+        self.dim = dim
+
+        self.size_params = 0
+        for group in self.param_groups:
+            for p in group['params']:
+                self.size_params += torch.numel(p)
+
+    def step(self, closure):
+
+        for group in self.param_groups:
+            beta1, beta2 = group['betas']
+
+            # Closure return the approximation for the gradient
+            grad_est = closure(self.size_params, group["mu"],
+                               self.N_samples, self.input_size,
+                               self.dim, self.sampler, self.gradient_mode)
+
+            for p, grad in zip(group['params'], grad_est):
+                state = self.state[p]
+
+                # Lazy state initialization
+                if len(state) == 0:
+                    # Exponential moving average of gradient values
+                    state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                    # Exponential moving average of squared gradient values
+                    state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                    # Maintains max of all exp. moving avg. of sq. grad. values
+                    state['max_exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                # Do the AdaMM updates
+                state['exp_avg'].mul_(beta1).add_(grad, alpha=(1.0 - beta1))
+                state['exp_avg_sq'].mul_(beta2).addcmul_(grad, grad, value=(1.0 - beta2))
+                state['max_exp_avg_sq'] = torch.maximum(state['max_exp_avg_sq'],
+                                                        state['exp_avg_sq'])
+
+                p.data.addcdiv_(state['exp_avg'], state['exp_avg_sq'].sqrt().add_(group['eps']), value=(-group['lr']))
+
+
+
+
+class ZO_SignSGD(torch.optim.Optimizer):
+    def __init__(self, params, input_size, gradient_mode='central', sampler='normal', N_samples = 5, dim=2, lr=1e-3,
+                  mu=1e-03, eps=1e-12):
+
+        defaults = dict(lr=lr, mu=mu, eps=eps)
+        super().__init__(params, defaults)
+        self.input_size = input_size
+        self.gradient_mode = gradient_mode
+        self.sampler = sampler
+        self.N_samples = N_samples
+        self.dim = dim
+
+        self.size_params = 0
+        for group in self.param_groups:
+            for p in group['params']:
+                self.size_params += torch.numel(p)
+
+    def step(self, closure):
+        for group in self.param_groups:
+            lr = group['lr']
+            for i, param in enumerate(group['params']):
+                grad_est = closure(self.size_params, group["mu"],
+                                   self.N_samples, self.input_size,
+                                   self.dim, self.sampler, self.gradient_mode)
+
+                param.data.add_(-lr * torch.sign(grad_est[i]))
